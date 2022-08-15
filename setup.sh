348dd4360a9d154f503a99ab1df806245325a177
Taka <takeru.kageyuki@gmail.com>
2022-08-15 22:45:33 +0700

    Improved the installation logic

--- setup.sh
+++ setup.sh
@@ -16,11 +16,12 @@ libadd() {
     local libadd_path=$(pm path com.miui.securityadd | cut -d ":" -f 2)
     mv $MODPATH/lib/libadd.so $MODPATH$libadd_path
     
-    local libadd_dalvik=$(pm path com.miui.securityadd | sed "s:.*/::")
-    find /data/dalvik-cache/*/*$libadd_dalvik* -delete &>/dev/null
+    local libadd_cache=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::")
+    find /data/dalvik-cache/*/*$libadd_cache* -delete &>/dev/null
+    find /data/system/package_cache/*/$libadd_cache* -delete &>/dev/null
     
-    local libadd_package=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::")
-    find /data/system/package_cache/*/$libadd_package* -delete &>/dev/null
+    local libadd_package=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::" | tr "[:upper:]" "[:lower:]")
+    find /data/system/package_cache/*/*$libadd_package* -delete &>/dev/null
 }
 
 libmain() {
@@ -29,7 +30,7 @@ libmain() {
     if [ $libmain_location != system ]; then
         pm clear com.miui.securitycenter &>/dev/null
         pm uninstall com.miui.securitycenter &>/dev/null
-    elif [ $libmain_version != 40000621 ]; then
+    elif [ $libmain_version != 40000623 ]; then
         pm clear com.miui.securitycenter &>/dev/null
     fi
     
@@ -39,11 +40,12 @@ libmain() {
     local libmain_path=$(pm path com.miui.securitycenter | cut -d ":" -f 2)
     mv $MODPATH/lib/libmain.so $MODPATH$libmain_path
     
-    local libmain_dalvik=$(pm path com.miui.securitycenter | sed "s:.*/::")
-    find /data/dalvik-cache/*/*$libmain_dalvik* -delete &>/dev/null
+    local libmain_cache=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::")
+    find /data/dalvik-cache/*/*$libmain_cache* -delete &>/dev/null
+    find /data/system/package_cache/*/$libmain_cache* -delete &>/dev/null
     
-    local libmain_package=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::")
-    find /data/system/package_cache/*/$libmain_package* -delete &>/dev/null
+    local libmain_package=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::" | tr "[:upper:]" "[:lower:]")
+    find /data/system/package_cache/*/*$libmain_package* -delete &>/dev/null
 }
 
 install_lib() { libadd; libmain; }
