6054ca448e0b2cf2363afcc7dc24a2b11663c319
Taka <takeru.kageyuki@gmail.com>
2022-08-15 22:47:59 +0700

    Improved the cache cleaner logic

--- uninstall.sh
+++ uninstall.sh
@@ -13,19 +13,21 @@ boot_completed() {
 }
 
 libadd() {
-    local libadd_dalvik=$(pm path com.miui.securityadd | sed "s:.*/::")
-    find /data/dalvik-cache/*/*$libadd_dalvik* -delete &>/dev/null
+    local libadd_cache=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::")
+    find /data/dalvik-cache/*/*$libadd_cache* -delete &>/dev/null
+    find /data/system/package_cache/*/$libadd_cache* -delete &>/dev/null
     
-    local libadd_package=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::")
-    find /data/system/package_cache/*/$libadd_package* -delete &>/dev/null
+    local libadd_package=$(pm path com.miui.securityadd | cut -d "." -f 1 | sed "s:.*/::" | tr "[:upper:]" "[:lower:]")
+    find /data/system/package_cache/*/*$libadd_package* -delete &>/dev/null
 }
 
 libmain() {
-    local libmain_dalvik=$(pm path com.miui.securitycenter | sed "s:.*/::")
-    find /data/dalvik-cache/*/*$libmain_dalvik* -delete &>/dev/null
+    local libmain_cache=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::")
+    find /data/dalvik-cache/*/*$libmain_cache* -delete &>/dev/null
+    find /data/system/package_cache/*/$libmain_cache* -delete &>/dev/null
     
-    local libmain_package=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::")
-    find /data/system/package_cache/*/$libmain_package* -delete &>/dev/null
+    local libmain_package=$(pm path com.miui.securitycenter | cut -d "." -f 1 | sed "s:.*/::" | tr "[:upper:]" "[:lower:]")
+    find /data/system/package_cache/*/*$libmain_package* -delete &>/dev/null
 }
 
 lib_cleanup() {
